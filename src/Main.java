4ca2ef2d78838b096dd658d2e795d01e39b75d58
honzasvacek <jan.svacek@student.gyarab.cz>
2022-09-25 08:15:02 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int a = 0;
-        int b = 1;
-        int c = 2;
+        Scanner scannerA = new Scanner(System.in);
+        Scanner scannerB = new Scanner(System.in);
+        Scanner scannerC = new Scanner(System.in);
+        System.out.println("write 3 integers:");
+        int a = scannerA.nextInt();
+        int b = scannerB.nextInt();
+        int c = scannerC.nextInt();
+        int middle = 0;
+
+        if(a>b){
+            if(a<c){middle = a;}
+            else if (b>c) {middle = b;}
+            else middle = c;
+
+        }
+        else{
+            if(a>c){middle = a;}
+            else if(b<c){middle = b;}
+            else middle = c;
+        }
+        System.out.println("The median is: " + middle);
     }
 }
